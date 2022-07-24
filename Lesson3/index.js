2fb627900b42e723d8958b0c11ad1e4f6505640b
Samiul Islam Niloy <samiul3030@gmail.com>
2022-07-25 01:23:53 +0600

    Initial

---
+++ Lesson3/index.js
@@ -0,0 +1,27 @@
+// os, path
+// const os = require("os");
+// const {totalmem, freemem} = require("os");
+
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+// console.log(totalmem());
+// console.log(freemem());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require("path");
+// console.log(path)
+
+// const extensionName = path.extname("index.html");
+// console.log(extensionName);
+
+// const joinName = path.join(__dirname + "/views");
+// console.log(joinName);
+
+const joinName = path.join(__dirname + "/../views");
+console.log(joinName);
