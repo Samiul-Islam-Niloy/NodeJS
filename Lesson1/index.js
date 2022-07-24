351eef4a1c066f7b0bca1f8404ae73b9e5c2d7cc
Samiul Islam Niloy <samiul3030@gmail.com>
2022-07-25 00:49:06 +0600

    Initial

---
+++ Lesson1/index.js
@@ -0,0 +1,6 @@
+const {getName} = require('./student');
+
+//  console.log(s1.getName());
+//  console.log(s1.getAge());
+//  console.log(s1.cgpa);
+console.log(getName())
---
+++ Lesson1/student.js
@@ -0,0 +1,19 @@
+exports.getName = () =>{
+    return "Samiul Islam";
+}
+
+const getAge = () =>{
+    return "23";
+}
+
+const cgpa = 3.553;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
